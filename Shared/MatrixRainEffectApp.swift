61769d20b757c35d4ff0abce58c6029cf8f6c152
ebibibibibi <xvipea0@gmail.com>
2022-02-11 07:22:40 +0900

    Initial Commit

---
+++ Shared/MatrixRainEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixRainEffectApp.swift
+//  Shared
+//
+//  Created by kotomi tahkahashi on 2022/02/11.
+//
+
+import SwiftUI
+
+@main
+struct MatrixRainEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
