502e841fa143a43cbb517e2480937e3d247b44f5
ebibibibibi <xvipea0@gmail.com>
2022-02-11 08:42:57 +0900

    GeometryReader{proxy inの中身。             let size = proxy.size
                HStack(spacing: 15){
                    //横に15ずつスペースを空けてViewGroupを作成
                    VStack{
    
                        //縦のViewGroup
                        //MARK: Iterating String
    
                        ForEach(0..<constant.count,id: \.self){index in
                            //文字列の数だけForEachを回す。
    
                            //Retriving Character at String
                            let character = Array(constant)[index]
                            //characterでArrayのインデックスを入れる。
    
                            Text(String(character))

--- Shared/View/MatrixRainView.swift
+++ Shared/View/MatrixRainView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct MatrixRainView: View {
     var body: some View {
-        GeometryReader{
-            
+        
+        GeometryReader{proxy in
+            let size = proxy.size
+            //ここの
+            Text(String("geometry: \(proxy.size.debugDescription)"))
+            //print(_:)関数は、引数に与えられたオブジェクトの文字列表現(textual representation)を出力
+            //debugDescriptionはプロパティ
+            HStack(spacing: 15){
+                //横に15ずつスペースを空けてViewGroupを作成
+                VStack{
+                    //縦のViewGroup
+                    //MARK: Iterating String
+                    ForEach(0..<constant.count,id: \.self){index in
+                        //文字列の数だけForEachを回す。
+                        //Retriving Character at String
+                        let character = Array(constant)[index]
+                        //characterでArrayのインデックスを入れる。
+                        Text(String(character))
+                    }
+                }
+            }
         }
     }
 }
