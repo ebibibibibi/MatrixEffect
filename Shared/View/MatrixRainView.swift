da57511b226d5e348a4f48b17c49292cc4adc24e
ebibibibibi <xvipea0@gmail.com>
2022-02-15 20:37:51 +0900

    流れる動きをoffsetとwithAnimationで作った。

--- Shared/View/MatrixRainView.swift
+++ Shared/View/MatrixRainView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct MatrixRainView: View {
+    @State var startAnimation: Bool = false
     var body: some View {
-        
+    
         GeometryReader{proxy in
             let size = proxy.size
             //ここの
-            Text(String("geometry: \(proxy.size.debugDescription)"))
+            //            Text(String("geometry: \(proxy.size.debugDescription)"))
+            //                .foregroundColor(Color.yellow)
             //print(_:)関数は、引数に与えられたオブジェクトの文字列表現(textual representation)を出力
             //debugDescriptionはプロパティ
             HStack(spacing: 15){
@@ -25,10 +27,27 @@ struct MatrixRainView: View {
                         //文字列の数だけForEachを回す。
                         //Retriving Character at String
                         let character = Array(constant)[index]
+                        //constantをArrayと見做す。
                         //characterでArrayのインデックスを入れる。
                         Text(String(character))
+                            .font(.custom("Matrix Code NFI", size: 25))
+                            .foregroundColor(Color("Green"))
+                        //.font
                     }
                 }
+                .offset(y: startAnimation ? 0: -600)
+//  -size.heightをduration (秒)で移動するということ？
+                 //startAnimationはBool
+            }
+            .padding(.horizontal)
+       
+        }
+        .onAppear {
+            //Moving Siowly down with linear Animation(線形のアニメーション)
+            withAnimation(.linear(duration: 12)){
+                // withAnimation(.linearで等速で移動。
+                startAnimation = true
+                //startAnimationがtureだったらアニメーションが始まる。
             }
         }
     }
