8b45bfc2f1eda7c8e8594e19cd882f9ff4ffccd2
ebibibibibi <xvipea0@gmail.com>
2022-02-19 08:24:00 +0900

    完成

--- Shared/View/MatrixRainView.swift
+++ Shared/View/MatrixRainView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct MatrixRainView: View {
     var body: some View {
-        
+        /*MatrixRainViewは画面全体のsizeの値をMatrixRainCharactersに渡している。
+         FOrEachでGeometry
+         For*/
         GeometryReader{proxy in
             let size = proxy.size
             //ここの
@@ -18,6 +20,8 @@ struct MatrixRainView: View {
             //print(_:)関数は、引数に与えられたオブジェクトの文字列表現(textual representation)を出力
             //debugDescriptionはプロパティ
             HStack(spacing: 15){
+                // 全画面を占めるまでエフェクトを繰り返す
+                // フォントサイズが25なので、width/fontSizeがカウントになる。
                 //横に15ずつスペースを空けてViewGroupを作成
                 ForEach(1...Int(size.width / 25),id: \.self){ _ in
                     MatrixRainCharacters(size: size)
@@ -25,7 +29,7 @@ struct MatrixRainView: View {
                     //size.width / 25 -> 1...12
                     //size.width / 100 -> 1,2,3
                     //size.width / 250 -> 1
-                    //size.width /Intの結果に応じて、画面をHStackとして分割しているということか？
+                    //size.width /Intの結果に応じて、画面をHStackとして分割している
                 }
             }
             .padding(.horizontal)
@@ -37,7 +41,7 @@ struct MatrixRainCharacters: View{
     //MARK: Animation Properties
     var size: CGSize
     @State var startAnimation: Bool = false
-    
+    @State var random: Int = 0
     var body: some View{
         VStack{
             //縦のViewGroup
@@ -45,16 +49,17 @@ struct MatrixRainCharacters: View{
             ForEach(0..<constant.count,id: \.self){index in
                 //文字列の数だけForEachを回す。
                 //Retriving Character at String
-                let character = Array(constant)[index]
+                let character = Array(constant)[getRandomIndex(index: index)]
                 //constantをArrayと見做す。
                 //characterでArrayのインデックスを入れる。
+                //ランダムな文字を取得
                 Text(String(character))
                     .font(.custom("Matrix Code NFI", size: 25))
                     .foregroundColor(Color("Green"))
                 //.font
             }
         }
-        //Fade like Animation Using Mask
+        //.maskを使ったフェードアニメーション
         .mask(alignment: .top){
             Rectangle()
                 .fill(
@@ -77,19 +82,39 @@ struct MatrixRainCharacters: View{
             // Animating
             // To look like its coming from Top
                 .offset(y: startAnimation ? size.height : -(size.height/2))
-            /*ここまでmaskの配下。
-             mask, LinearGradientで透明度の調整。
-             offsetで動きを追加。
-             */
         }
         .onAppear {
             //Moving Siowly down with linear Animation(線形のアニメーション)
             //Endless loop without re
-            withAnimation(.linear(duration: 12).repeatForever(autoreverses: false)){
+//            withAnimation(.linear(duration: 12).repeatForever(autoreverses: false)){
+            withAnimation(.linear(duration: 12).delay(.random(in: 0...2)).repeatForever(autoreverses: false)){
                 // withAnimation(.linearで等速で移動。
+                //アニメーションを遅らせるには、インスタンスメソッドdelay()を使う。
+                //上下で挙動を比較すると面白い。
                 startAnimation = true
                 //startAnimationがtureだったらアニメーションが始まる。
             }
+        }
+        // Timer
+        .onReceive(Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()) { _ in
+            
+            random = Int.random(in: 0..<constant.count)
+        }
+    }
+    
+    // タイマーを使ってランダムにキャラクターを変更することができる
+    func getRandomIndex(index: Int)->Int{
+        //let character = Array(constant)[getRandomIndex(index: index)]
+        // インデックスが範囲外にならないようにするために"-1"する。
+        let max = constant.count - 1
+        if (index + random) > max{
+            if (index - random) < 0{
+                return index
+            }
+            return (index - random)
+        }
+        else{
+            return (index + random)
         }
     }
 }
